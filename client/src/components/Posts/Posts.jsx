e089e7a5e963c06b399e5c8064d73b696fad8fb2
Ryan Guelzo <r.guelzo88@gmail.com>
2022-02-01 13:00:38 -0800

    Styles available to each component now.

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import useStyles from "./styles.js";
 import Post from "./Post/Post";
 
 const Posts = () => {
+  const classes = useStyles();
   return (
     <>
       <h1>POSTS</h1>
