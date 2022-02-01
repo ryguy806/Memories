4f189f20e121bc9cc9684c17f25e38aa8f1a5a1f
Ryan Guelzo <r.guelzo88@gmail.com>
2022-02-01 10:02:21 -0800

    Added imge and setup App front page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { AppBar, Container, Typography } from "@material-ui/core";
+import memories from "./images/memories.png";
 
 const App = () => {
   return (
-    <div>
-      <h1>App!</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+          <img src={memories} alt="memories" height="60" />
+        </Typography>
+      </AppBar>
+    </Container>
   );
 };
 
