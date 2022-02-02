c38fd482e2562a22e7bf049fe47127560b907b01
Ryan Guelzo <r.guelzo88@gmail.com>
2022-02-02 08:57:26 -0800

    Finished implementing Redux. Needs dispatchs.

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import App from "./App";
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDom.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
