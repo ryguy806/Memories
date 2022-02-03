dd7549a5f06915d34e3d8454809a139a41e6ed47
Ryan Guelzo <r.guelzo88@gmail.com>
2022-02-03 12:34:15 -0800

    Started setting up delete function.

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getPosts, createPost, updatePost } from "../controllers/posts.js";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} from "../controllers/posts.js";
 const router = express.Router();
 
 // http://localhost:5000/posts
@@ -7,5 +12,6 @@ const router = express.Router();
 router.get("/", getPosts);
 router.post("/", createPost);
 router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 export default router;
